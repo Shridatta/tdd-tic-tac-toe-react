3ae37b30e92526465c76a004117a47ff508f02af
Shridatta <sgz2@njit.edu>
2019-03-13 00:07:14 -0400

    Failing test :- when rendering the square should render the square div

---
+++ src/components/Square.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { shallow, configure, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Square from "./Square";
+
+configure({ adapter: new Adapter() });
+
+const square = shallow(<Square />);
+
+describe("when rendering the Square", () => {
+  it("should render the square div", () => {
+    expect(square.find(".squareDiv").exists()).toBeTruthy();
+  });
+});
