1556425ecfed31c30da6dbe0dbbf227c805d64df
Shridatta <sgz2@njit.edu>
2019-03-12 22:28:04 -0400

    Passing test :- when rendering the app board renders correctly

---
+++ src/components/Board.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class Board extends Component {
+  render() {
+    return <div className="board">tictactoe</div>;
+  }
+}
+
+export default Board;
