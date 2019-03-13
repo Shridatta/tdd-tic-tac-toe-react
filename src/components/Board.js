a08009f99ea2517906cf16885bcfff3611b958c1
Shridatta <sgz2@njit.edu>
2019-03-12 22:57:25 -0400

    Passing test :- initializes the board state slots with null

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import "./App.css";
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+    console.log(this.state);
+  }
   render() {
     return <div className="board">Board</div>;
   }
