c0afef86a9cd0901dc96d709bb37c6fb153214e4
Shridatta <sgz2@njit.edu>
2019-03-12 22:48:26 -0400

    Passing test :- contains tic tac toe heading;

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1 className="tictactoeHeader"> TicTacToe Game</h1>
         <Board className="boardComponent" />
       </div>
     );
