0194b18a141f07e4221f66abb69f5b65b37d91ad
Shridatta <sgz2@njit.edu>
2019-03-13 01:39:11 -0400

    Passing Test :- should render all the slots for the board

--- src/components/Board.js
+++ src/components/Board.js
@@ -13,16 +13,28 @@ class Board extends Component {
   }
 
   renderSlot(i) {
-    return <Square />;
+    return (
+      <Square
+        className="squareinit"
+        value={this.state.squares[i]}
+        slotNumber={i}
+      />
+    );
   }
   render() {
     return (
       <div className="board">
         Board <br />
         <div className="currentPlayer">The current player is</div>
-        <div className="row" />
-        <div className="row">Row 2</div>
-        <div className="row">Row 3</div>
+        <div className="row">
+          {this.renderSlot(0)} {this.renderSlot(1)} {this.renderSlot(2)}
+        </div>
+        <div className="row">
+          {this.renderSlot(3)} {this.renderSlot(4)} {this.renderSlot(5)}
+        </div>
+        <div className="row">
+          {this.renderSlot(6)} {this.renderSlot(7)} {this.renderSlot(8)}
+        </div>
       </div>
     );
   }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,6 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
 const Square = props => {
   return <div className="squareDiv">Squares</div>;
 };
