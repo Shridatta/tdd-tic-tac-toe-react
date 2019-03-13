e67b97353a75f72e7ad8a8ea2e1d8fffa4381386
Shridatta <sgz2@njit.edu>
2019-03-13 01:40:04 -0400

    Rendering board with index numbers

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,6 +1,10 @@
 import React from "react";
 const Square = props => {
-  return <div className="squareDiv">Squares</div>;
+  return (
+    <div className="squareDiv" className={`square${props.slotNumber}`}>
+      {props.slotNumber}
+    </div>
+  );
 };
 
 export default Square;
