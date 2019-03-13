61c96de8f50376e4cde1b2018a1f4805a84a46a9
Shridatta <sgz2@njit.edu>
2019-03-12 22:06:09 -0400

    creating components folder and adding components in that

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { shallow, configure } from "enzyme";
-import App from "./App";
+import App from "../components/App";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
